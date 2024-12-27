054f22fa332520e4e69f332d157ab890c65a9a7f
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-27 11:42:45 +0100

    add all tasks

--- frontend/src/pages/AllTasksPage.jsx
+++ frontend/src/pages/AllTasksPage.jsx
@@ -1,5 +1,42 @@
+import { useState, useEffect } from 'react'
+
 export default function AllTasksPage() {
+
+    // logic
+    const [tasks, setTasks] = useState([]);
+
+    const url = `http://localhost:3007/categories/tasks`;
+
+    // fetch data
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data);
+                setTasks(data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
     return (
-        <h1>All tasks page</h1>
+        <>
+            <div className="d-flex flex-column align-items-center justify-content-center py-5">
+
+                {tasks ? tasks.map(task => (
+                    <div className="card text-center" key={task.id} style={{ width: "18rem", margin: "5px" }}>
+                        <div className="d-flex align-items-center ps-1">
+                            <i className="bi bi-circle text-muted m-0 p-0" style={{ verticalAlign: "middle" }}></i>
+                            <p className="m-0 p-2">{task.title}</p>
+                        </div>
+                    </div>
+
+                )) : (
+                    <p>Loading...</p>
+                )}
+
+            </div>
+        </>
     )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -17,7 +17,6 @@ export default function HomePage() {
                     <CompletedButton />
                 </div>
 
-
                 <CategoriesCard />
 
             </div>
