ba762810e3d7c6a79731f071f9b61c2c0b354bd7
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-27 12:00:41 +0100

    add helmet provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { HelmetProvider } from "react-helmet-async"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,7 +7,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <HelmetProvider>
     <App />
-  </StrictMode>,
+  </HelmetProvider>,
 )
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import CategoriesCard from "../components/CategoriesCard";
 import AllTasksButton from "../components/button/AllTasksButton";
 import CompletedButton from "../components/button/CompletedButton";
@@ -6,6 +7,9 @@ export default function HomePage() {
     // render
     return (
         <>
+            <Helmet>
+                <title>Home page - To Do List</title>
+            </Helmet>
             <div className="container py-4">
 
                 <div className="d-flex justify-content-center mb-4">
