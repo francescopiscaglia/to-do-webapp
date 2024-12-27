5c651420182de0272fbfa29ef82f1d24ea638be4
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-27 09:42:08 +0100

    add fetch data for all categories

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react"
+
 export default function HomePage() {
+
+    // logic
+
+    // state
+    const [categories, setCategories] = useState([]);
+
+    const url = `http://localhost:3007/categories`
+
+    // fetch data
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setCategories(data);
+            })
+    }, []);
+
+    // render
     return (
         <>
-            <div>Home page</div>
+            {categories ? categories.map(category => (
+                <div key={category.id}>
+                    <h2>{category.name}</h2>
+                    <p>{category.description}</p>
+                </div>
+            )) : (
+                <p>Loading...</p>
+            )}
         </>
     )
 }
